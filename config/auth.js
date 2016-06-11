8749a210ae0b66551c3ffe1db5958e0df09528ee
Seth Bergman <seth.atxwebs@gmail.com>
2016-06-10 21:25:21 -0500

    first

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '1634179146904876',
+        'clientSecret'    : '8f85b4de1606d032f48f6bba7766baf2',
+        'callbackURL'     : 'http://dash3.stackriot.com/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'GeSP7s3Y4LbEKN1y4LqTgybF1',
+        'consumerSecret'     : 'hkXTVgvbqfZ9HKRvOo6fHLcnaVnLVqUYb5lHTrCRHxeDZOxgQD',
+        'callbackURL'        : 'http://dash3.stackriot.com/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : '704451403113-2e25k3vqmtpipf7lhn0jkhanapie0u76.apps.googleusercontent.com',
+        'clientSecret'     : 'dmD1_MqoJRUB2F9O2vwSVpf_',
+        'callbackURL'      : 'http://dash3.stackriot.com/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://sethbergman:dash2user@ds019063.mlab.com:19063/dash2'
+
+};
