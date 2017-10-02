b7196129f19d024beec97ebf504528440d85393a
Seth Bergman <seth.atxwebs@gmail.com>
2017-10-02 06:03:55 -0500

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,25 +3,25 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1721239734790857',
         'clientSecret'    : '4e382ab8f2176c0418de8a7c7681bb60',
-        'callbackURL'     : 'http://dashboard-ejs.stackriot.com/auth/facebook/callback'
+        'callbackURL'     : 'https://dashboard-ejs.stackriot.com/auth/facebook/callback'
     },
 
     'githubAuth' : {
         'clientID'        : 'd511dcdcb8e6f14946e1',
         'clientSecret'    : '253fcb69955ceb42ad50a47ecc120e3a9f08941a',
-        'callbackURL'     : 'http://dashboard-ejs.stackriot.com/auth/github/callback'
+        'callbackURL'     : 'https://dashboard-ejs.stackriot.com/auth/github/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : '4A04gIbsT2PmPZ248l3enVDdH',
         'consumerSecret'     : 'KrqDIIpTLuxUqqdGAfoXPZETHG4IM7ctk2BKQNeqlSA3iXCXfB',
-        'callbackURL'        : 'http://dashboard-ejs.stackriot.com/auth/twitter/callback'
+        'callbackURL'        : 'https://dashboard-ejs.stackriot.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : '814038177529-d9460ecipv6aac9ff6mb66abh0ugsr32.apps.googleusercontent.com',
         'clientSecret'     : '1wEylmDeCYbATFF9BTtDnXVX',
-        'callbackURL'      : 'http://dashboard-ejs.stackriot.com/auth/google/callback'
+        'callbackURL'      : 'https://dashboard-ejs.stackriot.com/auth/google/callback'
     }
 
 };
